151e1ab411614f945bd377864873ed479b3c5156
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-04-07 09:58:28 +0530

    changes in README

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,7 +1,7 @@
 const ContactCard = (props) => {
     return (
         <div className="contact-card">
-            <img src="./logo192.png" width="120px" />
+            <img src="./logo192.png" alt="logo" width="120px" />
             <div className="user-details">
                 <p>Name: {props.name}</p>
                 <p>email: {props.email}</p>
